5eac4240b2a19e508af9711e35df3930665b9035
hy <mintlib66@gmail.com>
2022-09-01 20:55:12 +0900

    드래그 드랍 아이템 교환  기능구현

--- script.js
+++ script.js
@@ -41,4 +41,50 @@ function createList() {
       listItems.push(listItem)
       draggable_list.appendChild(listItem)
     })
+  addDragEvent()
+}
+
+function addDragEvent() {
+  const draggable = document.querySelectorAll('.draggable')
+  const dragListItem = document.querySelectorAll('.draggable-list li')
+
+  draggable.forEach(item => {
+    item.addEventListener('dragstart', dragStart)
+  })
+  dragListItem.forEach(item => {
+    item.addEventListener('dragover', dragOver)
+    item.addEventListener('drop', dragDrop)
+    item.addEventListener('dragenter', dragEnter)
+    item.addEventListener('dragleave', dragLeave)
+  })
+}
+
+function dragStart() {
+  dragStartIndex = this.closest('li').getAttribute('data-index')
+  console.log('start ' + dragStartIndex)
+}
+function dragOver(e) {
+  e.preventDefault()
+}
+function dragEnter() {
+  this.classList.add('over')
+}
+function dragLeave() {
+  this.classList.remove('over')
+}
+function dragDrop() {
+  const dragEndIndex = this.getAttribute('data-index')
+  console.log('drop ' + dragEndIndex)
+  swapItems(dragStartIndex, dragEndIndex)
+
+  this.classList.remove('over')
+}
+function swapItems(fromIndex, toIndex) {
+  const fromItem = listItems[fromIndex].querySelector('.draggable')
+  const toItem = listItems[toIndex].querySelector('.draggable')
+  console.log(fromIndex + '->' + toIndex)
+  console.log(fromItem, toItem)
+
+  listItems[fromIndex].appendChild(toItem)
+  listItems[toIndex].appendChild(fromItem)
 }
